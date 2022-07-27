20f8827c848253b8ce79010c92b343fe5a1520a9
Denis Korobitsin <deniskorobicyn@users.noreply.github.com>
2022-07-28 01:02:44 +0500

    Add diff fetch

--- security_checker/domain/vsc/github.py
+++ security_checker/domain/vsc/github.py
@@ -1,19 +1,15 @@
 import os
-from typing import List
+from urllib.request import urlopen
 from github import Github, GithubIntegration
-from github.Comparison import Comparison
-from security_checker.domain.config import Config
 
 from security_checker.domain.vsc import VSC
 
-
-class Github(VSC):
+class GithubVSC(VSC):
     def __init__(self, owner, repo_name) -> None:
-        config = Config._instance
-        app_id = config.github.app_id
+        app_id = "222372"
         # Read the bot certificate
         with open(
-                os.path.normpath(os.path.expanduser(config.github.pem_path)),
+                os.path.normpath(os.path.expanduser("./secrets/private-key.pem")),
                 'r'
         ) as cert_file:
             app_key = cert_file.read()
@@ -26,13 +22,15 @@ class Github(VSC):
         self.github = Github(
             login_or_token=git_integration.get_access_token(
                 git_integration.get_installation(owner, repo_name).id
-        ).token
-        self.repo = self.github.get_repo(f"{self.owner}/{self.repo_name}")
-    )
+            ).token
+        )
+        self.repo = self.github.get_repo(f"{owner}/{repo_name}")
 
 
-    def fetch_diff(self, head, base) -> Comparison:
-        return self.repo.compare(head, base)
+    def fetch_diff(self, head, base) -> str:
+        comparasion = self.repo.compare(head, base)
+        diff_url = comparasion.diff_url
+        return urlopen(diff_url).read().decode('utf-8')
 
     def update_status(self, status) -> None:
         self.repo.get_commit(sha=self.sha).create_status(
